d607435f51ee3ae1d775fa617438a13ebe48ae65
TSUCHIMOTOAOTO\aotok <aotokinoko@icloud.com>
2025-11-27 12:11:46 +0900

    背景画像追加しました。 Prefab背景の座標の位置と幅高さを変更しました。 Component幅高さを変更しました。

--- HEW_2025/Source/ECS/Prefabs/PrefabBackGround.cpp
+++ HEW_2025/Source/ECS/Prefabs/PrefabBackGround.cpp
@@ -25,17 +25,21 @@ void RegisterBackGroundPrefab(PrefabRegistry& registry)
             /// TransformÅiscale ÇÕÇªÇÃÇ‹Ç‹égÇ¶ÇÈÇÊÇ§Ç…ÇµÇƒÇ®Ç≠Åj
             auto& tr = w.Add<TransformComponent>(e, sp.position, sp.rotationDeg, sp.scale);
             tr.position.x = 35.0f;
+            tr.position.y = 45.0f;
             tr.scale = { 1.0f,1.0f,1.0f };
 
             // Spriteîwåi
             auto& spb = w.Add<BackGroundComponent>(e);
-            spb.alias = "tex_newbackground"; // îwåiâÊëú
-            spb.width = 65.0f;
-            spb.height = 65.0f;
+            spb.alias = "tex_w1920h2160"; // îwåiâÊëú
+            //"tex_newbackground"
+            //"tex_w1920h1080"
+            //"tex_w1920h2160"
+            spb.width = 76.8f;     //76.8f          64.0f
+            spb.height = 86.4f;    //172.8f ,86.4f   64.0f
             spb.originX = 0.5f;
             spb.originY = 0.5f;
             spb.layer = -10;      // îwåiå„ñ 
-            spb.visible = true;  // ï\é¶
+            spb.visible = true;   // ï\é¶
 
             return e;
         }
