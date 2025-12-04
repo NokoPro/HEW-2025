d2d7c8cf9d9a9c3520802fd6887de9acc1ee6fce
yukur <yukura2009@icloud.com>
2025-12-05 03:36:22 +0900

    test

--- HEW_2025/Source/ECS/Prefabs/PrefabResultUi.cpp
+++ HEW_2025/Source/ECS/Prefabs/PrefabResultUi.cpp
@@ -25,19 +25,12 @@ void RegisterResultUiPrefab(PrefabRegistry& registry)
             /// TransformÅiscale ÇÕÇªÇÃÇ‹Ç‹égÇ¶ÇÈÇÊÇ§Ç…ÇµÇƒÇ®Ç≠Åj
             auto& tr = w.Add<TransformComponent>(e, sp.position, sp.rotationDeg, sp.scale);
             tr.position = sp.position;
-            tr.position.x = sp.position.x;
-            tr.position.y = sp.position.y;  // yç¿ïWí≤êÆÇµÇ‹ÇµÇΩ
             tr.scale = sp.scale;
-            
-           
-            const char* fileName[] = 
-            { "tex_w1920h2160" ,
-                "testui",
-            };
+
+
             // Spriteîwåi
             auto& spb = w.Add<ResultUiComponent>(e);
             spb.alias = sp.modelAlias;
-            spb.alias = fileName[(int)sp.scale.z]; // îwåiâÊëú
 
             // í«â¡ÇµÇΩîwåiâÊëúñºëO
             //"tex_newbackground" newBackGround.jpg
@@ -45,12 +38,11 @@ void RegisterResultUiPrefab(PrefabRegistry& registry)
             //"tex_w1920h2160"    w1920h2160.jpg
 
                                    // ééÇµÇΩíl
-            spb.width = 16.8f+ sp.scale.x;     //76.8f  ,64.0f
-            spb.height = 16.4f+ sp.scale.y;    //172.8f ,86.4f   64.0f
+            spb.width = 16.8f + sp.scale.x;     //76.8f  ,64.0f
+            spb.height = 16.4f + sp.scale.y;    //172.8f ,86.4f   64.0f
             spb.originX = 0.5f + sp.position.x;
             spb.originY = 0.5f + sp.position.y;
             spb.layer = -100;      // îwåiå„ñ 
-            spb.layer = -10;      // îwåiå„ñ 
             spb.visible = true;   // ï\é¶
 
             return e;
